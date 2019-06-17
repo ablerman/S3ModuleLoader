2d0dd0a5bf60cd3ecde0c3ec8f8d972e26b7fbe2
Sandy Lerman <alex@ablerman.com>
2019-06-17 14:42:16 -0400

    Getting ready to send to pypi

--- s3_module_loader/s3_module_loader/s3_loader.py
+++ s3_module_loader/s3_module_loader/s3_loader.py
@@ -1,48 +1,9 @@
 import sys
 import imp
-from importlib.abc import MetaPathFinder
 from importlib.abc import Loader
 import boto3
 
 s3 = boto3.client('s3')
-
-
-class S3Finder(MetaPathFinder):
-    """Module finder that locates python modules in s3"""
-    def __init__(self, bucket):
-        self.bucket = bucket
-        self.loaders = {}
-        self._keys = None
-
-    @property
-    def keys(self):
-        if self._keys is None:
-            results = s3.list_objects_v2(Bucket=self.bucket)
-            self._keys = [k['Key'] for k in results['Contents']]
-        return self._keys
-
-    def is_s3_module(self, fullname):
-        parts = fullname.split('.')
-        if len(parts) == 1:
-            path = parts[0]
-        else:
-            path = '/'.join(parts)
-        for k in self.keys:
-            if k.startswith(path):
-                return True
-        return False
-
-    def find_module(self, fullname, path=None):
-        if not self.is_s3_module(fullname):
-            # print(f"This doesn't seem to be a package in s3: {fullname}")
-            raise ImportError
-
-        print(f'S3Finder::find_module {fullname} {path}')
-        key = fullname
-        if key not in self.loaders:
-            self.loaders[key] = S3Loader(path, self.bucket)
-        return self.loaders[key]
-
 
 class S3Loader(Loader):
     """Module Loader for python modules in S3"""
@@ -139,6 +100,4 @@ class S3Loader(Loader):
             exec(source, mod.__dict__)
 
         return mod
-        # path = find_notebook(fullname, self.path)
-
 
